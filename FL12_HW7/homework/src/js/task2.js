705e26a0c9320d07b60bed9cd3ca26349b257332
Denis Beleshev <niskadevla@gmail.com>
2019-12-26 13:07:34 +0200

    made part 2 Task7

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -10,47 +10,57 @@ if(!isPlay) {
 //Step 2 and 3
 let minNum = 0;
 let maxNum = 8;
+let maxNumInc = 4;
 let userNum;
 let attempts = 3;
 let totalPrize = 0;
-let prizes = [100, 50, 25];
-// let prize_1 = 100;
-// let prize_2 = 50;
-// let prize_3 = 25;
-let leftAttempts;
-let isLose = false;
-// let curPrize;
+let prize_1 = 25;
+let prize_2 = 50;
+let prize_3 = 100;
+let prizes = [prize_1, prize_2, prize_3];
+let prizeInc = 2;
+let attemptsLeft;
 
 if(isPlay) {
   //Do a loop 'do while' while the User is plaing
+  quit:
   do {
     let randNum = Math.floor( minNum + Math.random() * (maxNum + 1 - minNum) );
-    // curPrize = prize_1;
-    leftAttempts = attempts;
-
-    for(; leftAttempts > 0; leftAttempts--) {
+    attemptsLeft = attempts;
+    //Loop for 3 attempts
+    for(; attemptsLeft > 0; attemptsLeft--) {
       userNum = prompt(`\
-        Choose a roulette pocket number from 0 to 8 \n\
-        Attempts left: ${leftAttempts} \n\
+        Choose a roulette pocket number from 0 to ${maxNum} \n\
+        Attempts left: ${attemptsLeft} \n\
         Total prize: ${totalPrize}$ \n\
-        Possible prize on current attempt: ${prizes[leftAttempts - 1]}$`, '');
+        Possible prize on current attempt: ${prizes[attemptsLeft - 1]}$`, '');
 
+      //If cancel quit the game
       if(!userNum) {
-        break;
+        break quit;
       }
-
+      //If User entered data:
       if(userNum) {
-        if(randNum == userNum) {
-          totalPrize += prizes[leftAttempts - 1];
-        } else {
-          isLose = true;
-          alert('Thank you for your participation.');
+        //String to Number
+        userNum = +userNum;
+        //If won:
+        if(randNum === userNum) {
+          totalPrize += prizes[attemptsLeft - 1];
+          alert(`\
+            Congratulation, you won!\n\
+            Your prize is: ${totalPrize}$`);
+          isPlay = confirm('Do you want to continue?');
+          maxNum += maxNumInc;
+          prizes = prizes.map(prize => prize * prizeInc);
+          break;
+        } else if(attemptsLeft === 1) { //If lose:
+          alert(`\
+            Thank you for your participation.\n\
+            Your prize is: ${totalPrize}`);
           isPlay = confirm('Do you want to play again?');
         }
       }
-
     }
 
-  } while (userNum || isLose);
-
+  } while (isPlay);
 }
