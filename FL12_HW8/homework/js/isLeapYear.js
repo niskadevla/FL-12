883a59c6b6e27a61a3926f9fc95cb9ccbc56dffb
Denis Beleshev <niskadevla@gmail.com>
2019-12-28 16:24:04 +0200

    Edit Task8 part 7

--- FL12_HW8/homework/js/isLeapYear.js
+++ FL12_HW8/homework/js/isLeapYear.js
@@ -1,30 +1,17 @@
 function isLeapYear(date) {
-  let newDate;
+  let newDate = new Date(date);
 
-  console.log(typeof date);
-
-  if(typeof date === 'string') {
-    newDate = Date.parse(date);
-  } else if(typeof date === 'number') {
-    newDate = new Date(date);
-  }
-
-  if(!newDate) {
+  let year = newDate.getFullYear();
+  if(!year) {
     return 'invalid Date';
   }
 
-  console.log(newDate);
-
-  let year = newDate.getFullYear();
-
-  if( (year % 4 === 0) && (year % 400 === 0) && (year % 100 !== 0) ) {
+  if( (year % 4 === 0) && (year % 100 !== 0) ||
+      (year % 400 === 0) ) {
      return `${year} is a leap year`;
   } else {
     return `${year} is not a leap year`;
   }
 }
 
-// let date = new Date(1212131);
-// let date = Date.parse('2020-01-01T00:00:00');
-console.log(isLeapYear('2020-01-01T00:00:00'));
-// console.log(date.getFullYear());
+console.log( isLeapYear('2021-01-15 13:00:00') );
